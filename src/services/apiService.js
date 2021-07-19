8d94d82dd79fc7e5df75723a3daf1fad2190975b
Tetiana1386 <tanyashostak13@gmail.com>
2021-07-19 13:04:07 +0300

    HW_3_image_finder

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,13 +1,31 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api';
-const API_KEY = '21693934-b739dad2632fdbf7884e4d0a2';
+axios.defaults.baseURL = 'https://pixabay.com/api';
+axios.defaults.params = {
+  key: '21693934-b739dad2632fdbf7884e4d0a2',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  per_page: 12,
+};
 
 const fetchImages = async (query, page) => {
-  const { data } = await axios.get(
-    `${BASE_URL}/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
-  );
+  const { data } = await axios.get('', {
+    params: {
+      q: query,
+      page,
+    },
+  });
   return data.hits;
 };
 
 export default fetchImages;
+
+//const BASE_URL = 'https://pixabay.com/api';
+//const API_KEY = '21693934-b739dad2632fdbf7884e4d0a2';
+
+//const fetchImages = async (query, page) => {
+//const { data } = await axios.get(
+//`${BASE_URL}/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+//);
+//return data.hits;
+//};
